d47bac338ec04bacda52dee00f2e4d5d6265ac83
David Jimenez Calvo <david.jimenez19@gmail.com>
2014-02-06 14:58:02 +0000

    Init commit
    Ran yo marionette:compositeview peopleview --itemview personview
    --create-all and yo marionette:router router-name

---
+++ test/spec/testSuite.js
@@ -0,0 +1,12 @@
+define(function() {
+	'use strict';
+
+	/* return an array of specs to be run */
+	return {
+		specs: ['spec/exampleTest.js',
+		'spec/routers/router-person.js',
+		'spec/views/composite/peopleview.js',
+		'spec/views/item/personview.js'
+		]
+	};
+});
